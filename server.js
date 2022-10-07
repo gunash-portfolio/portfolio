abfb8a3746fd65172e17c5ff30146455d83cbedc
Gunash Farzaliyev <goddontlike@Gunashs-MacBook-Pro.local>
2022-10-08 01:10:58 +0400

    contact 80% is finished and server confugiration added

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server running"));
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
+const contactEmail = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "djsisijdi@gmail.com",
+    pass: "sdjasjida",
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
